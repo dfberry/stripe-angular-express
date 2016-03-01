341cac8c464d7dafb86f5a99071b0682d1c4b3b4
dfberry <dinaberry@outlook.com>
2016-03-01 14:43:51 -0800

    [wip] figuring out jasmine tests

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,5 @@
 exports.$myappconfig = function() {
+    
     return {
         stripePublishableKey: 'pk_test_ArJPMDKT6lF2Ml4m4e8ILmiP',
         donationDescription: 'Donation for XYZ'
--- public/js/test.js
+++ public/js/test.js
@@ -8,8 +8,30 @@
         })
     });
 
-    it("should return a property", inject(function () {        
-        console.log(service.stripePublishableKey);
+    it("should return a property stripePublishableKey", inject(function () {        
         expect(service.stripePublishableKey).toBe('pk_test_ArJPMDKT6lF2Ml4m4e8ILmiP');
-    }))
+    }));
+ 
+    it("should return a property donationDescription", inject(function () {        
+        expect(service.donationDescription).toBe('Donation for XYZ');
+    })); 
+});
+
+describe("$myappconst", function (service) {
+    beforeEach(module('stripe-app.components'));
+
+    var service;
+    beforeEach(function () {
+        angular.mock.inject(function ($injector) {
+            service = $injector.get('$myappconst');
+        })
+    });
+
+    it("months should return 12 items", inject(function () {        
+        expect(service.months.length).toBe(12);
+    }));
+ 
+    it("months_default should return months[10]", inject(function () {        
+        expect(service.months_default).toBe(service.months[10]);
+    })); 
 });
