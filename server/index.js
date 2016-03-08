aa0d89c9313f7c312e7e575b1c249a43291182ba
dfberry <dinaberry@outlook.com>
2016-03-08 10:56:31 -0800

    separating server from initialization

--- server/index.js
+++ server/index.js
@@ -1,20 +1,7 @@
 'use strict';
+var config = require('./config.json')
+    , app = require('./server');
 
-var express = require('express');
-var wagner = require('wagner-core');
-var path = require('path');
-
-require('./dependencies')(wagner);
-
-var app = express();
-
-app.use(require('morgan')());
-
-app.get(['/'], function (req, res) {
-  res.sendFile(path.join(__dirname + '/../public/default.html'));
+var server = app().listen(config.port.stage, function(){
+    console.log("listening on port " + config.port.stage);
 });
-app.use('/api/v1', require('./api')(wagner));
-app.use('/public', express.static(__dirname + '/../public', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
-
-app.listen(3000);
-console.log('Listening on port 3000!');
---
+++ server/server.js
@@ -0,0 +1,29 @@
+'use strict';
+var express = require('express');
+var wagner = require('wagner-core');
+var path = require('path');
+require('./dependencies')(wagner);
+
+module.exports = function() {
+  var app = express();
+  app.use(require('morgan')('combined'));
+
+    app.get(['/'], function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/default.html'));
+    });
+    
+    app.get(['/charge'], function (req, res){
+
+    });
+    
+    app.use('/api/v1', require('./api')(wagner));
+    app.use('/public', express.static(__dirname + '/../public', {   maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
+
+  return app;
+};
+
+
+
+
+
+
